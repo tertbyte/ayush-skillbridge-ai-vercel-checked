fe6a1acd50d9e4705722070fd1555c17dc1c7366
tertbyte <sachumusthu@gmail.com>
2026-09-07 12:03:52 +0530

    Add Madhu Mitra demo-name override

---
+++ public/name-override.js
@@ -0,0 +1,21 @@
+(() => {
+  const FROM = /\bAnanya(?:\s+Menon)?\b/g;
+  const TO = "Madhu Mitra";
+
+  const replaceText = (node) => {
+    if (node.nodeType === Node.TEXT_NODE) {
+      if (FROM.test(node.nodeValue)) {
+        FROM.lastIndex = 0;
+        node.nodeValue = node.nodeValue.replace(FROM, TO);
+      }
+      return;
+    }
+    if (node.nodeType === Node.ELEMENT_NODE && node.tagName !== "SCRIPT" && node.tagName !== "STYLE") {
+      node.childNodes.forEach(replaceText);
+    }
+  };
+
+  const run = () => replaceText(document.getElementById("root") || document.body);
+  new MutationObserver(run).observe(document.documentElement, { childList: true, subtree: true, characterData: true });
+  run();
+})();
